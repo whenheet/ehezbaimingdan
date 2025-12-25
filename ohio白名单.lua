c5060cf65fea1db8f2fd32ae39646b254096b776
whenheet <3579175593@qq.com>
2025-12-25 17:05:45 +0800

    Update ohio白名单.lua

--- ohio白名单.lua
+++ ohio白名单.lua
@@ -147,6 +147,7 @@ Button.MouseButton1Click:Connect(function()
        ["tysfh45fh"] = {user = "xiaoruo9", expire = "2030-12-24"},
        ["ihdjk45kl"] = {user = "cheems070828", expire = "2030-12-24"},
        ["wygsnvko"] = {user = "zdclll2", expire = "2030-12-24"},
+       ["djcnsujd45"] = {user = "18hbibmv", expire = "2030-12-24"},
        ["h5Z6D4D2"] = {user = "12345678xto", expire = "2030-12-31"}
         -- 可复制上面的格式，添加更多卡密
     }
